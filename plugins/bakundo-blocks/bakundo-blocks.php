86aac4463e8e05f19b8ed13e07e78e9ebaf5cd3b
Mads Andersen <mosimuuh@gmail.com>
2026-03-23 14:47:11 +0100

    test

---
+++ plugins/bakundo-blocks/bakundo-blocks.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Bakundo Blocks
+ * Description: Custom Gutenberg blocks til Bakundo-sites
+ * Version: 1.0
+ * Author: Mads Bakundo
+ */
+
+function bakundo_register_blocks() {
+    register_block_type( __DIR__ . '/bakundo-hero/build' );
+}
+add_action( 'init', 'bakundo_register_blocks' );
---
+++ plugins/bakundo-blocks/bakundo-hero/build/bakundo-hero/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b032298053b31c5ba07d');
